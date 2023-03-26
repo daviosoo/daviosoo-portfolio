33b80ecc8b2ff87c35f08e6123e8b2d4fe52f44e
David Osorio <davosorio404@gmail.com>
2023-03-26 00:35:48 -0500

    chore: links

--- src/app/layout.js
+++ src/app/layout.js
@@ -8,9 +8,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
-        {children}
-      </body>
+      <body className="py-24">{children}</body>
     </html>
   );
 }
